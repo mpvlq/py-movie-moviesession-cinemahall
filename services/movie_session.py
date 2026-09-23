09f815d974cd835a3d77e01296dd3813d7b6a550
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-09-23 15:29:15 +0200

    add missing functions again

--- services/movie_session.py
+++ services/movie_session.py
@@ -21,3 +21,26 @@ def create_movie_session(
         cinema_hall=cinema_hall,
         movie=movie,
     )
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: str = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> MovieSession:
+    updated_movie_session = MovieSession.objects.get(pk=session_id)
+    if show_time:
+        updated_movie_session.show_time = show_time
+    if movie_id:
+        updated_movie_session.movie = Movie.objects.get(pk=movie_id)
+    if cinema_hall_id:
+        updated_movie_session.cinema_hall = CinemaHall.objects.get(
+            pk=cinema_hall_id
+        )
+    updated_movie_session.save()
+    return updated_movie_session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.get(pk=session_id).delete()
